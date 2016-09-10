d075daa5a513cd46441ac2692cf8abd78357d621
Max McKenzie <max@lemondigits.com>
2016-09-10 17:22:07 +0200

    changed files to jsx as per style guide and created basic listing component

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Data from './components/Data';
+import parser from './helpers/Parser';
+import logger from './helpers/logger';
+
+const url = 'http://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/json/api-with-examples.json';
+
+logger.info(parser(url));
+
+const items = [
+  {
+    title: 'Some Thing',
+  },
+  {
+    title: 'Some other thing',
+  },
+];
+
+ReactDOM.render(
+  <Data items={items} />,
+  document.getElementById('app')
+);
