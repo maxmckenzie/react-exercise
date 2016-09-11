39c37268189a312f8aa06753f56bbcf1fc3086d9
Max McKenzie <max@lemondigits.com>
2016-09-11 21:24:20 +0200

    omitted un nessisary comments

--- src/components/App.spec.jsx
+++ src/components/App.spec.jsx
@@ -2,12 +2,8 @@
 import { shallow } from 'enzyme';
 import App from './App.jsx';
 
-// let json = require('./schemas/sample.json');
-// const items = Object.keys(json).map(x => json[x]);
-
 describe('App', () => {
   const props = {
-    //TODO add correct items props here
     items: [{ title: 'x' }, { title: 'y' }],
   };
   it('shows two elements', () => {
