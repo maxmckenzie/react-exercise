ff783a91fc8bd690c30cff6af3a0a98be7f28a81
Max McKenzie <max@lemondigits.com>
2016-09-11 17:35:33 +0200

    refactored request responce

--- src/helpers/Parser.js
+++ src/helpers/Parser.js
@@ -1,21 +1,11 @@
-import http from 'http';
+import Request from 'requestify';
 import logger from './logger';
 
 function Parser(src) {
-  http.get(src, (res) => {
-    let body = '';
-
-    res.on('data', (chunk) => {
-      body += chunk;
+  Request.get(src)
+    .then((responce) => {
+      logger.info(responce.getBody);
     });
-
-    res.on('end', () => {
-      const responce = JSON.parse(body);
-      logger.info('Got a response: ', responce.picture);
-    });
-  }).on('error', (e) => {
-    logger.info('Got an error: ', e);
-  });
 }
 
 export default Parser;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import Data from './components/Data.jsx';
 import parser from './helpers/Parser';
 import logger from './helpers/logger';
 
-const url = 'http://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/json/api-with-examples.json';
+const url = 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v2.0/json/api-with-examples.json';
 
-// logger.info(parser(url));
+logger.info(parser(url));
 
 const items = [
   {
