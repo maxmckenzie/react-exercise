48713da02a8b8ceeec54527203f0fa86cb79682b
Max McKenzie <max@lemondigits.com>
2016-09-13 14:17:51 +0200

    removed json table from paths comp

--- src/components/Paths.jsx
+++ src/components/Paths.jsx
@@ -4,7 +4,6 @@ import logger from '../helpers/logger';
 function Paths({ paths = [] }) {
   logger.info(paths);
   const items = Object.keys(paths).map(x => paths[x]);
-  return <JsonTable rows={ items } />;
   // return (
   //   <div className="paths">paths, paths__url, paths__method, paths__description, paths__parameters, paths__responce, paths__tags</div>
   // )
