28358d266e4fe2b6d1d5cc79645a437aff570e93
Max McKenzie <max@lemondigits.com>
2016-09-20 16:34:55 +0100

    paths comp update

--- src/components/Paths.jsx
+++ src/components/Paths.jsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import logger from '../helpers/logger';
 
-function Paths({ paths = [] }) {
-  logger.info(paths);
-  const items = Object.keys(paths).map(x => paths[x]);
-  // return (
-  //   <div className="paths">paths, paths__url, paths__method, paths__description, paths__parameters, paths__responce, paths__tags</div>
-  // )
+function getFields(method) {
+  logger.info(method);
+  return Object.keys(method).map((fieldKey, fieldIndex) => (
+    <div key={fieldIndex}>
+    {fieldKey}
+    </div>
+  ));
 }
 
-export default Paths;
+function getMethods(path) {
+  return Object.keys(path).map((methodKey, methodIndex) => (
+    <div key={methodIndex}>
+      <h3>{ methodKey }</h3>
+      { getFields(path[methodKey]) }
+    </div>
+  ));
+}
+
+function getPaths(paths) {
+  return Object.keys(paths).map((pathKey, pathIndex) => (
+    <div key={pathIndex}>
+      <h2>{ pathKey }</h2>
+      { getMethods(paths[pathKey]) }
+    </div>
+  ));
+}
+
+export default function Paths({ paths = {} }){
+  return (
+    <div>
+      <h1>Paths</h1>
+      { getPaths(paths) }
+    </div>
+  )
+}
